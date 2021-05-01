77d52ae5bea2800391dca73b2760160946184228
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-05-01 13:45:23 +0200

    :art: add format config

--- public/script.js
+++ public/script.js
@@ -3,11 +3,11 @@ const header = document.getElementById("scrollc");
 // J'en fait une fonction pour pouvoir l'appeler au chargement de la page car
 // le scoll n'est pas forcément en haut au chargement.
 function onWindowScroll(event) {
-  if (window.pageYOffset < 5) {
-    header.classList.remove("scrolled");
-  } else {
-    header.classList.add("scrolled");
-  }
+	if (window.pageYOffset < 5) {
+		header.classList.remove("scrolled");
+	} else {
+		header.classList.add("scrolled");
+	}
 }
 
 window.addEventListener("scroll", onWindowScroll);

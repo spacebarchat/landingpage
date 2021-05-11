edbdce37df6dcdbe31972f95fd74f8348953a37f
Stylix58 <lateman-jpeg@outlook.fr>
2021-05-11 19:37:30 +0200

    replace with rewrite

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/fosscord/fosscord-landingpage-next)
+[![Github Pages Publish](https://github.com/fosscord/fosscord-landingpage-next/actions/workflows/build.yml/badge.svg)](https://github.com/fosscord/fosscord-landingpage-next/actions/workflows/build.yml)
+# [Fosscord landing page](https://fosscord.github.io/fosscord-landingpage-next)
+The official website of Fosscord.
+
+## Setup
+First, clone the repository:
+
+`git clone https://github.com/fosscord/fosscord-landingpage-next/edit/master/README.md`
+
+### Install
+For testing:
+`npm install`
+
+For production:
+`npm install --prod`
+
+### CLI usage
+For bundle the website:
+`npm run build`
+
+For render the site in real-time:
+`npm run watch`
+
+These commands will build the website in the `dist` folder.
---
+++ index.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const fs = require("fs");
+const fse = require("fs-extra");
+const pug = require("pug");
+
+// Remove dist folder
+if (fse.ensureDir("./dist") == true) {
+	fse.remove("./dist", (err) => {
+		if (err) throw err;
+		console.log("Successfully removed dist folder");
+	})
+}
+
+// Copy public folder into dist folder
+fse.copy("./public", "./dist", (err) => {
+	if (err) throw err;
+	console.log("Successfully copied public folder into dist folder");
+});
+
+// Copy Bootstrap CSS and JS and Normalize.CSS into dist folder
+fse.copy("./node_modules/bootstrap/dist/css/bootstrap.min.css", "./dist/bootstrap.min.css", (err) => {
+	if (err) throw err;
+	console.log("Successfully copied bootstrap.min.css from node_modules folder to dist folder");
+});
+
+fse.copy("./node_modules/bootstrap/dist/js/bootstrap.min.js", "./dist/bootstrap.min.js", (err) => {
+	if (err) throw err;
+	console.log("Successfully copied bootstrap.min.js from node_modules folder to dist folder");
+});
+
+// Compile pug views into html files
+function compile(template, finalFile, values) {
+	const temp = pug.compileFile("views/" + template + ".pug");
+	if (finalFile == "index.html") {
+		fs.writeFile("dist/index.html", temp(values), (err) => {
+                	if (err) throw err;
+                	console.log("Successfully compiled views/" + template + ".pug into dist/" + finalFile);
+        	});
+	} else {
+		fs.writeFile("dist/" + template + "/"  + finalFile, temp(values), (err) => {
+			if (err) throw err;
+			console.log("Successfully compiled views/" + template + ".pug into dist/" + finalFile);
+		});
+	}
+}
+
+console.log("You can see the builded content in " + path.resolve(__dirname, "dist/index.html"));
+compile("home", "index.html", { title: "Fosscord - Chat platform" });

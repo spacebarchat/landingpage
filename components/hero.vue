4fdc301c41ca6ce3e21157949cf608c5808c3431
xnacly <matteogropp@protonmail.com>
2021-07-22 13:38:59 +0200

    removed badge

--- components/hero.vue
+++ components/hero.vue
@@ -3,9 +3,9 @@
     <div class="container px-xl-0">
       <div class="mb-md-5 row justify-content-center align-items-center">
         <div class="col-md-12 col-xl-8 text-center">
-          <div class="pb-15 color-heading f-22 medium">
+          <!--<div class="pb-15 color-heading f-22 medium">
             <div><span class="badge bg-fosscord text-white">fosscord.com</span></div>
-          </div>
+          </div>-->
           <h2 class="text-adaptive">A new way to communicate across different platforms</h2>
         </div>
       </div>

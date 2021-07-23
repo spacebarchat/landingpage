cd5b101032946b0048680c1809366d563332ac4e
xnacly <matteogropp@protonmail.com>
2021-07-23 14:25:23 +0200

    added missing href to warning banner

--- components/warning.vue
+++ components/warning.vue
@@ -2,7 +2,8 @@
   <div class="warning">
     <p>
       Fosscord is still a work in progress. If you're an advanced user see
-      <a>here</a> for a guide on how to get started.
+      <a href="https://docs.fosscord.com/contributing.html">here</a> for a guide
+      on how to get started.
     </p>
   </div>
 </template>

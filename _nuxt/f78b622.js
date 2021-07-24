4a2a908b9a3a3e35ab169028913432b9f9018fa3
xNaCly <matteogropp@protonmail.com>
2021-07-24 11:15:18 +0000

    Deploying to gh-pages from @ fosscord/fosscord-landingpage@293983c9b18de29b53dd07bc25cefb6bb72b66a3 🚀

--- _nuxt/f78b622.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{241:function(n,e,d){"use strict";d.r(e);var l=d(34),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);

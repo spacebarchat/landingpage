c9382a7ad7bf27c1e27b094a3a198d24b43422ad
stefan080106 <stefan.arnold.programmierung@gmail.com>
2021-08-03 19:19:23 +0200

    :sparkles: add Donation Ribbon

--- components/warning.vue
+++ components/warning.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="warning">
+    <div class="ribbon">
+      <span>
+        <a href="https://opencollective.com/fosscord">
+          Donate
+        </a>
+      </span>
+    </div>
     <p>
       Fosscord is still a work in progress. If you're an advanced user see
       <a href="https://docs.fosscord.com/contributing.html">here</a> for a guide

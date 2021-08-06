39f02a73c2c8d09a9570451dbcbbcf54e8d426e6
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-06 22:49:35 +0200

    Update warning.vue

--- components/warning.vue
+++ components/warning.vue
@@ -9,7 +9,7 @@
     </div>
     <p>
       Fosscord is still a work in progress. If you're an advanced user see
-      <a href="https://docs.fosscord.com/contributing.html">here</a> for a guide
+      <a href="https://docs.fosscord.com/contributing/">here</a> for a guide
       on how to get started.
     </p>
   </div>

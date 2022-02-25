20d197ee7eaaabe3e9786db9e173e17609ec82b7
Natsuki.HX <58345270+AToska21@users.noreply.github.com>
2022-02-25 15:25:48 +0100

    added color to a tag (#43)
    added color to a tag because it was invisible and that really annoying, vue.js supports css right

--- components/warning.vue
+++ components/warning.vue
@@ -1,4 +1,29 @@
 <template>
+  <style>
+  a:link {
+  color: blue;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:visited {
+  color: purple;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:hover {
+  color: pink;
+  background-color: transparent;
+  text-decoration: underline;
+}
+
+a:active {
+  color: blue;
+  background-color: transparent;
+  text-decoration: underline;
+}
+  </style>
   <div class="warning">
     <div class="ribbon">
       <span>

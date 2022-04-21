d11cc27b9e711049f7c403df5a98b5274c8d4a3a
hypax <me@hypax.wtf>
2022-04-21 21:17:18 +0200

    link colors
    This should fix links looking blue, pink, etc. and just display them in the defined and wanted color.

--- components/warning.vue
+++ components/warning.vue
@@ -1,41 +1,14 @@
 <template>
-  <style>
-  a:link {
-  color: blue;
-  background-color: transparent;
-  text-decoration: none;
-}
-
-a:visited {
-  color: purple;
-  background-color: transparent;
-  text-decoration: none;
-}
-
-a:hover {
-  color: pink;
-  background-color: transparent;
-  text-decoration: underline;
-}
-
-a:active {
-  color: blue;
-  background-color: transparent;
-  text-decoration: underline;
-}
-  </style>
   <div class="warning">
     <div class="ribbon">
       <span>
-        <a href="https://opencollective.com/fosscord">
-          Donate
-        </a>
+        <a href="https://opencollective.com/fosscord"> Donate </a>
       </span>
     </div>
     <p>
       Fosscord is still a work in progress. If you're an advanced user, read the
-      <a href="https://docs.fosscord.com/setup/server/">Documentation</a> for a guide on how
-      to get started.
+      <a href="https://docs.fosscord.com/setup/server/">Documentation</a> for a
+      guide on how to get started.
     </p>
   </div>
 </template>

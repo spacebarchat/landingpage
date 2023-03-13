7a6445c0d75c40f5903ac6dbe5d168a09fc2dfc7
Gustavo Ribeiro <contato@gustavoribeiro.io>
2023-03-13 03:39:25 -0300

    feat: added nav menu

--- src/routes/[[lang]]/+layout.svelte
+++ src/routes/[[lang]]/+layout.svelte
@@ -6,17 +6,20 @@
 
 	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 
+	import { Twitter, Instagram, Github } from 'lucide-svelte';
+
 	import { LL } from '$lib/i18n/i18n-svelte';
 </script>
 
 <div class="max-w-7xl justify-center items-center flex mx-auto">
-	<AppShell slotHeader="my-3">
+	<AppShell>
 		<svelte:fragment slot="header">
 			<AppBar
 				gridColumns="grid-cols-3"
 				slotDefault="place-self-center"
 				slotTrail="place-content-end"
 				background="transparent"
+				padding="py-10"
 			>
 				<svelte:fragment slot="lead">
 					<img
@@ -25,13 +28,24 @@
 						class="w-48"
 					/>
 				</svelte:fragment>
-				<nav>Navigation</nav>
+				<nav>
+					<div class="btn-group variant-ringed">
+						<a href="/">{$LL.SECTIONS.ABOUT()}</a>
+						<a href="/instances">{$LL.SECTIONS.INSTANCES()}</a>
+						<a href="/contribute">{$LL.SECTIONS.CONTRIBUTE()}</a>
+						<a href="/roadmap">{$LL.SECTIONS.ROADMAP()}</a>
+						<a href="https://docs.fosscord.com/" target="_blank">{$LL.SECTIONS.DOCS()}</a>
+						<a href="https://twitter.com/fosscord_/"><Twitter /></a>
+						<a href="https://instagram.com/fosscord/" rel="me"><Instagram /></a>
+						<a href="https://github.com/fosscord/fosscord/"><Github /></a>
+					</div>
+				</nav>
 				<svelte:fragment slot="trail">
-					<button type="button" class="btn variant-filled"
+					<a href="/" class="btn variant-filled"
 						>{$LL.COMMON.SIGN_IN()}
 						<span class="badge variant-filled-warning ml-2"
 							>{$LL.NAVBAR.DEMO_ALPHA_WARNING_BADGE()}</span
-						></button
+						></a
 					>
 				</svelte:fragment>
 			</AppBar>

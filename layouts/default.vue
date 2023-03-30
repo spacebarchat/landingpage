00fc1ee3615d32cb2c253a2b1db00099b47c4587
TheArcaneBrony <myrainbowdash949@gmail.com>
2023-03-30 19:08:09 +0200

    Add Matrix Space link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 
 		<div>
 			<h1>We are currently undergoing a major rebrand.</h1>
-			<h2>Join our <a href="https://discord.gg/Ms5Ev7S6bF">Discord guild</a> for more information.</h2>
+			<h2>Join our <a href="https://matrix.to/#/!OovtSBQaxNmuBWlQzV:feline.support/?via=feline.support&via=rory.gay&via=envs.net">Matrix Space</a> or <a href="https://discord.gg/Ms5Ev7S6bF">Discord guild</a> for more information.</h2>
 		</div>
 
 	<!-- <warning />
